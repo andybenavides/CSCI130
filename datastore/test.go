c51f821bdd7fcae226d5006cc651c55ed4fae729
Andy Benavides <benavides_0548076@mail.fresnostate.edu>
2015-04-06 12:06:09 -0700

    Datastore Assignment
    unfinished working copy

---
+++ datastore/test.go
@@ -0,0 +1,88 @@
+package guestbook
+
+import (
+	"html/template"
+	"net/http"
+	"time"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+)
+
+type Greeting struct {
+	Author  string
+	Content string
+	Date    time.Time
+}
+
+func init() {
+	http.HandleFunc("/", root)
+	http.HandleFunc("/sign", sign)
+}
+
+func requireUserLogin(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	// Require user login
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+}
+
+// guestbookKey returns the key used for all guestbook entries.
+func guestbookKey(c appengine.Context) *datastore.Key {
+	// The string "default_guestbook" here could be varied to have multiple guestbooks.
+	return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	requireUserLogin(w, r)
+
+	q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := signTemplate.Execute(w, greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var (
+	guestbookForm []byte
+	signTemplate  = template.Must(template.ParseFiles("guestbookform.html"))
+)
+
+func sign(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	g := Greeting{
+		Content: r.FormValue("content"),
+		Date:    time.Now(),
+	}
+	if u := user.Current(c); u != nil {
+		g.Author = u.String()
+	}
+	// We set the same parent key on every Greeting entity to ensure each Greeting
+	// is in the same entity group. Queries across the single entity group
+	// will be consistent. However, the write rate to a single entity group
+	// should be limited to ~1/second.
+	key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+	_, err := datastore.Put(c, key, &g)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
+}
